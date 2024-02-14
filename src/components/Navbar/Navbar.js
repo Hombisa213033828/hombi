eec0cbe8c8692d5761d920514cc02e3b998c42d2
Hombisa Dyasi <hombisadyasi@gmail.com>
2024-02-14 08:51:32 +0200

    Creating navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import'./Navbar.css'
+
+export default function Navbar() {
+  return (
+    <div className='navbar-header'>
+        <div className='nav-container'>
+        <ul>
+            <li>HOME</li>
+            <li>ABOUT ME</li>
+            <li>JOURNEY</li>
+            <li>PROJECTS</li>
+            <li>CONTACTS</li>
+        </ul>
+        </div>
+    </div>
+  )
+}
