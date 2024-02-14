ee011c02e155c02b5e31f5fdccefb16eb6188d06
Hombisa Dyasi <hombisadyasi@gmail.com>
2024-02-14 16:29:56 +0200

    updating cards in a journey page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+// import Journey from './components/Journey/Journey'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      {/* <Journey/> */}
     </div>
   );
 }
