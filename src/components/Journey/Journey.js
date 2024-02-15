eea02eac3aca54d9c28bc2f66df75f21aa8ba644
Hombisa Dyasi <hombisadyasi@gmail.com>
2024-02-15 10:07:55 +0200

    creating q-cards

--- src/components/Journey/Journey.js
+++ src/components/Journey/Journey.js
@@ -1,16 +1,40 @@
-// import React from 'react'
-// import'./Journey.css'
+import React from 'react';
+import'./Journey.css';
+
+function Journey(){
+  return(
+    <div className="journey">
+      
+    <div className="card">
+        <img src="Wsu2.png" height={"50"}/>
+        <div className="card__content">
+          <p className="card__title">Walter Sisulu</p>
+          <p classNameName="card__title">Walter Sisulu</p>
+          <p> Enrolled: 2013/01</p>
+          <p> Completed: 2016/12</p>
+          <p> Course: N.Diploma in Business Analysis</p>
+          </div>
+      </div>
+
+      <div className="card">
+        <img src="cput.png" height={"80"}/>
+        <div className="card__content">
+          <p className="card__title">Cape Peninsula</p>
+          <p classNameName="card__title">Walter Sisulu</p>
+          <p> Enrolled: 2013/01</p>
+          <p> Completed: 2016/12</p>
+          <p> Course: N.Diploma in Business Analysis</p>
+          </div>
+      </div>
+    </div>
+  )
+}
+export default Journey
 
 
-<div class="card-1">
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-    <path d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path></svg>
-  <div class="card__content">
-    <p class="card__title">Walter Sisulu</p>
-    <p class="card__description"> Enrolled: 2013/01</p>
-    <p class="card__description"> Completed: 2016/12</p>
-    <p class="card__description"> Course: N.Diploma in Business Analysis</p>
-    
-  </div>
-</div>
-
+// <div classNameName="card__content">
+// <p classNameName="card__title">Walter Sisulu</p>
+// <p> Enrolled: 2013/01</p>
+// <p> Completed: 2016/12</p>
+// <p> Course: N.Diploma in Business Analysis</p>
+// </div>
