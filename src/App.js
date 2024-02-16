bcec16169f9b33e87e207e085547acc597fa3890
Hombisa Dyasi <hombisadyasi@gmail.com>
2024-02-16 10:38:57 +0200

    creating cards for education page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Education from './components/Education/Education';
-// import Experience from './components/Experience/experience';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <Home /> */}
+      <Home />
       <Education/>
-      {/* <Experience/> */}
     </div>
   );
 }
--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,41 +1,109 @@
 import React from 'react';
 import'./Education.css';
+import SalesPic from './images/sales-logo.png';
+
 
 function Education(){
   return(
-    <div className="journey">
-      {/* <h2>EDUCATTION</h2> */}
-      
-    <div className="card">
+    <div className="experience">
+      <div className='top-content'>
+      <h3>Education</h3>
+        <div className='top-cards'>
+           <div className='card-1'>
+           
+           <div className="card">
         <img src="Wsu2.png" height={"100"}/>
         <div className="card__content">
           <p className="card__title">Walter Sisulu</p>
-          {/* <p classNameName="card__title">Walter Sisulu</p> */}
-          <p> Year Enrolled: 2013/01</p>
+         
           <p> Year Completed: 2016/12</p>
           <p> Course: N.Diploma in Business Analysis</p>
           </div>
-      </div>
-
-      <div className="card">
+         </div>
+           
+            </div>
+           <div className='card-2'>
+           
+           <div className="card">
         <img src="cput.png" height={"100"}/>
         <div className="card__content">
           <p className="card__title">Cape Peninsula</p>
-          {/* <p classNameName="card__title">Cape Peninsula</p> */}
+       
           <p> Year Enrolled: 2019/01</p>
           <p> year Completed: 2019/12</p>
           <p> Course: B-Tech Information and Technology Management </p>
+          </div>
+        </div> 
+           
+           </div>
+        </div>
+      </div>
+
+      <div className='bottom-content'>
+        <h3>Certificates</h3>
+          <div className='certificates'>
+            <div className='sales'>
+
+            <div class="cert-card">
+                <div class="card-img">
+                  <img src={SalesPic} alt='Sales force picture' id='sale-pic' />
+                </div>
+                <div class="card-info">
+                <p class="text-title">Sales Force </p>
+                <p class="text-body">Product description and details</p>
+                </div>
+                <div class="card-footer">
+                
+                <div class="card-button">
+                  <button>View Certificates</button>
+                 </div>
+                </div>
+            </div>
+
+            </div>
+            <div className='Mulesoft'>
+
+            <div class="cert-card">
+                <div class="card-img">
+                  <img src={SalesPic} alt='Sales force picture' id='sale-pic' />
+                </div>
+                <div class="card-info">
+                <p class="text-title">Sales Force </p>
+                <p class="text-body">Product description and details</p>
+                </div>
+                <div class="card-footer">
+                
+                <div class="card-button">
+                  <button>View Certificates</button>
+                 </div>
+                </div>
+            </div>
+
+            </div>
+            <div className='academics'>
+
+            <div class="cert-card">
+                <div class="card-img">
+                  <img src={SalesPic} alt='Sales force picture' id='sale-pic' />
+                </div>
+                <div class="card-info">
+                <p class="text-title">Sales Force </p>
+                <p class="text-body">Product description and details</p>
+                </div>
+                <div class="card-footer">
+                
+                <div class="card-button">
+                  <button>View Certificates</button>
+                 </div>
+                </div>
+            </div>
+            
+            </div>
           </div>
       </div>
     </div>
   )
 }
-export default Education
+export default Education;
 
 
-// <div classNameName="card__content">
-// <p classNameName="card__title">Walter Sisulu</p>
-// <p> Enrolled: 2013/01</p>
-// <p> Completed: 2016/12</p>
-// <p> Course: N.Diploma in Business Analysis</p>
-// </div>
