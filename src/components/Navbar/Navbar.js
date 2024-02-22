848b58f7d88c8ab82b5f6227d57c503b10c17387
Hombisa213033828 <hombisadyasi@gmail.com>
2024-02-22 16:59:31 +0200

    changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Navbar.css';
-import { FaBars } from 'react-icons/fa'; // Import the hamburger menu icon
+
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,15 +13,15 @@ export default function Navbar() {
     <div className='navbar-header'>
       <div className={`nav-container ${isOpen ? 'active' : ''}`}>
         <ul className={isOpen ? 'active' : ''}>
-          <li><a href="#home"></a>Home</li>
-          <li><a href="#Education"></a>Education</li>
-          <li><a href="#Portfolio"></a>Portfolio</li>
-          <li><a href="#ContactForm"></a>ContactForm</li>
+          <li>Home</li>
+          <li>Education</li>
+          <li>Portfolio</li>
+          <li>ContactForm</li>
         </ul>
-      </div>
-      <div className='menu-icon' onClick={toggleMenu}>
-        <FaBars />
       </div>
     </div>
   );
 }
+
+
+
