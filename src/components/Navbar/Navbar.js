8c08872c44374806f8356f74a14cd5fa221726c4
Hombisa213033828 <hombisadyasi@gmail.com>
2024-02-23 08:53:11 +0200

    linking the Navbar with the pages

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
+import { Link, animateScroll as scroll } from 'react-scroll';
 import './Navbar.css';
-
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,15 +13,62 @@ export default function Navbar() {
     <div className='navbar-header'>
       <div className={`nav-container ${isOpen ? 'active' : ''}`}>
         <ul className={isOpen ? 'active' : ''}>
-          <li>Home</li>
-          <li>Education</li>
-          <li>Portfolio</li>
-          <li>ContactForm</li>
+          <li>
+            <Link
+              activeClass="active"
+              to="home"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              onClick={toggleMenu}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              activeClass="active"
+              to="Education"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              onClick={toggleMenu}
+            >
+              Education
+            </Link>
+          </li>
+          <li>
+            <Link
+              activeClass="active"
+              to="portfolio"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              onClick={toggleMenu}
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link
+              activeClass="active"
+              to="ContactForm"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              onClick={toggleMenu}
+            >
+              ContactForm
+            </Link>
+          </li>
         </ul>
       </div>
     </div>
   );
 }
-
 
 
